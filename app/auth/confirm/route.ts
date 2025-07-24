6b70cb0f258b8fffc5c91e41925062aef3d7b887
Miguel Almeida <miguelvalmeida26@gmail.com>
2025-07-24 14:55:37 +0100

    Layout improvements

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -23,7 +23,6 @@ export async function GET(request: NextRequest) {
     }
   }
 
-  // return the user to an error page with some instructions
-  redirectTo.pathname = "/auth/auth-code-error";
+  redirectTo.pathname = "/login";
   return NextResponse.redirect(redirectTo);
 }
--- components/login-form.tsx
+++ components/login-form.tsx
@@ -7,7 +7,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { AlertCircle, Loader2 } from "lucide-react";
 import Link from "next/link";
 
-import { login, loginWithGoogle } from "@/lib/actions";
+import { login } from "@/lib/actions";
 
 import { Button } from "./ui/button";
 import {
@@ -21,6 +21,7 @@ import { Input } from "./ui/input";
 import { Form, FormItem, FormField, FormLabel, FormControl } from "./ui/form";
 import { Alert, AlertDescription } from "./ui/alert";
 import { PasswordInput } from "./password-input";
+import { LoginWithGoogle } from "./login-with-google";
 
 const formSchema = z.object({
   email: z.email(),
@@ -103,17 +104,7 @@ export function LoginForm() {
             Ou continua com
           </span>
         </div>
-        <form action={loginWithGoogle}>
-          <Button variant="outline" className="w-full">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-              <path
-                d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                fill="currentColor"
-              />
-            </svg>
-            Iniciar sessão com o Google
-          </Button>
-        </form>
+        <LoginWithGoogle variant="login" />
         <div className="grid gap-2 justify-center">
           <div className="text-center text-sm">
             Não tens uma conta?{" "}
@@ -121,12 +112,15 @@ export function LoginForm() {
               Registar
             </Link>
           </div>
-          <Link
-            href="/forgot-password"
-            className="ml-auto text-sm underline-offset-4 hover:underline"
-          >
-            Esqueceste-te da palavra-passe?
-          </Link>
+          <div className="text-center text-sm">
+            Esqueceste-te da palavra-passe?{" "}
+            <Link
+              href="/forgot-password"
+              className="underline underline-offset-4"
+            >
+              Recuperar
+            </Link>
+          </div>
         </div>
       </CardContent>
     </Card>
