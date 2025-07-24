88abf3b4b6d79143d234f96c156abdeb227ca602
Miguel Almeida <miguelvalmeida26@gmail.com>
2025-07-24 18:44:18 +0100

    Use drawer in mobile for password requirements

--- components/password-tooltip.tsx
+++ components/password-tooltip.tsx
@@ -1,22 +1,62 @@
+"use client";
+
 import { HelpCircle } from "lucide-react";
 
+import { useIsMobile } from "@/hooks/use-mobile";
+
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "./ui/drawer";
+
+function PasswordRequirementsList() {
+  return (
+    <ul className="lg:text-xs space-y-0.5">
+      <li>• Mínimo de 8 caracteres</li>
+      <li>• Pelo menos uma letra minúscula</li>
+      <li>• Pelo menos uma letra maiúscula</li>
+      <li>• Pelo menos um número</li>
+      <li>• Pelo menos um símbolo (@$!%*?&)</li>
+    </ul>
+  );
+}
 
 export function PasswordTooltip() {
+  const isMobile = useIsMobile();
+
+  if (isMobile) {
+    return (
+      <Drawer>
+        <DrawerTrigger>
+          <HelpCircle size={16} className="text-gray-400 " />
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>Requisitos da palavra-passe</DrawerTitle>
+          </DrawerHeader>
+          <div className="p-4">
+            <PasswordRequirementsList />
+          </div>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+
   return (
     <Tooltip>
-      <TooltipTrigger asChild>
-        <HelpCircle className="h-4 w-4 text-gray-400 hover:text-gray-600 cursor-help" />
+      <TooltipTrigger>
+        <HelpCircle
+          size={16}
+          className="text-gray-400 hover:text-gray-600 cursor-help"
+        />
       </TooltipTrigger>
       <TooltipContent side="right" className="max-w-xs space-y-1">
         <p className="font-medium text-xs">Requisitos da palavra-passe:</p>
-        <ul className="text-xs space-y-0.5">
-          <li>• Mínimo de 8 caracteres</li>
-          <li>• Pelo menos uma letra minúscula</li>
-          <li>• Pelo menos uma letra maiúscula</li>
-          <li>• Pelo menos um número</li>
-          <li>• Pelo menos um símbolo (@$!%*?&)</li>
-        </ul>
+        <PasswordRequirementsList />
       </TooltipContent>
     </Tooltip>
   );
